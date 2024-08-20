bbeacd3563c50dcfdf1533d7269e72281b1eebd5
Vinicius <viniciusmarchete@gmail.com>
2024-08-20 13:28:08 -0300

    Finalização do projeto Carro

--- Carro/lib/Carro.java
+++ Carro/lib/Carro.java
@@ -8,26 +8,42 @@ public class Carro {
     // Criando método para acelerar o carro
     void acelerar() {
         if (ignicaoLigada) {
+            System.out.println(divisor);
             System.out.println("O carro está acelerando!");
+            System.out.println(divisor);
         } else {
+            System.out.println(divisor);
             System.out.println("O carro está desligado. Ligue a ignição primeiro.");
+            System.out.println(divisor);
         }
         
     }
-
+    String divisor = "*********************************";
     // Criando método para ligar a ignição
     void ligarIgnicao() {
+        if (ignicaoLigada==true){
+            System.out.println(divisor);
+            System.out.println("O carro já está ligado!");
+            System.out.println(divisor);
+        } else{
         ignicaoLigada = true;
+        System.out.println(divisor);
         System.out.println("O carro foi ligado!");
+        System.out.println(divisor);
+    }
     }
 
     // Criando método para desligar a ignição
     void desligarIgnicao() {
-        if (ignicaoLigada) {
-        ignicaoLigada = false;
-        System.out.println("O carro foi desligado.");
-        } else {
+        if (ignicaoLigada==false) {
+            System.out.println(divisor);
             System.out.println("O carro já está desligado.");
+            System.out.println(divisor);
+        } else {
+            ignicaoLigada = false;
+            System.out.println(divisor);
+            System.out.println("O carro foi desligado.");
+            System.out.println(divisor);
         }
     }
 }
--- Carro/lib/Main.java
+++ Carro/lib/Main.java
@@ -19,20 +19,31 @@ public class Main{
             meuCarro.modelo = respostamodelo;
             meuCarro.ano = respostaano;
 
+            String divisor = "*********************************";
+            System.out.println(divisor);
             System.out.println("Seu carro é um " + meuCarro.modelo + " Na cor " + meuCarro.cor + " do ano de "+ meuCarro.ano);
+            System.out.println(divisor);
 
-            System.out.println("O que gostaria de fazer? \n1. Ligar o Carro\n2. Acelerar o Carro\n3.Desligar o Carro");
-            int respostapg1 = scanner.nextInt();
+            int respostapg1=0;
 
-            if (respostapg1 == 1) { // Agora está correto
-                meuCarro.ligarIgnicao();
-            } else if (respostapg1 == 2) {
-                meuCarro.acelerar();
-            } else if (respostapg1 == 3) {
-                meuCarro.desligarIgnicao();
-            } else {
-                System.out.println("Opção inválida.");
+            while (respostapg1!=4) {
+                System.out.println("O que gostaria de fazer? \n1. Ligar o Carro\n2. Acelerar o Carro\n3.Desligar o Carro\n4.Finalizar Simulação");
+                respostapg1 = scanner.nextInt();
+                if (respostapg1 == 1) { // Agora está correto
+                    meuCarro.ligarIgnicao();
+                } else if (respostapg1 == 2) {
+                    meuCarro.acelerar();
+                } else if (respostapg1 == 3) {
+                    meuCarro.desligarIgnicao();
+                } else if (respostapg1 == 4) {
+                    System.out.println(divisor);
+                    System.out.println("Simulação finalizada!");
+                    System.out.println(divisor);
+                } else {
+                    System.out.println("Opção inválida.");
+                }
             }
+
 
             scanner.close();
         }
